3ec8d16a8e32b0c7f5943af9e635a1886d8d659c
CIAN DALLY <cpdally3@gmail.com>
2023-12-01 15:08:10 -0600

    Initialized file system n mkfs.wfs.c with superblock and root directory entry

--- mkfs.wfs.c
+++ mkfs.wfs.c
@@ -1,1 +1,68 @@
 #include "wfs.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+static int init_filesystem(const char *path) {
+    // Open the file for read-write, create if not exists, truncate to zero length
+    int fd = open(path, O_RDWR | O_CREAT | O_TRUNC, 0644);
+    if (fd == -1) {
+        perror("Error opening file");
+        return -1;
+    }
+
+    // Initialize the superblock
+    struct wfs_sb superblock = {
+        .magic = WFS_MAGIC,
+        .head = sizeof(struct wfs_sb) + sizeof(struct wfs_log_entry)  // Start of the next available space after the superblock
+    };
+
+    // Write the superblock to the file (The beginning of the log)
+    if (write(fd, &superblock, sizeof(struct wfs_sb)) == -1) {
+        perror("Error writing superblock (The beginning of the log)");
+        close(fd);
+        return -1;
+    }
+
+    // Create the root log entry
+    struct wfs_log_entry root_entry = {
+        .inode = {
+            .inode_number = 0,  // Assuming root has inode number 0
+            .deleted = 0,
+            .mode = S_IFDIR,    // Root is a directory
+            // TODO?: Set other inode fields as needed
+        }
+    };
+
+    // Write the root log entry to the file (Appending to the log)
+    if (write(fd, &root_entry, sizeof(struct wfs_log_entry)) == -1) {
+        perror("Error writing root log entry (Appending to the log)");
+        close(fd);
+        return -1;
+    }
+
+    // Close the file
+    close(fd);
+
+    printf("Filesystem initialized successfully at %s\n", path);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <disk_path>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    const char *disk_path = argv[1];
+
+    // Initialize the filesystem
+    if (init_filesystem(disk_path) == -1) {
+        fprintf(stderr, "Failed to initialize filesystem.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
